372307a156878e12592a51b589c5ac6ac3d4b1fb
Jeff <horusofoz@gmail.com>
2018-04-24 21:13:17 +0930

    * Moved movement to separate method * Started on rotation

--- ArgonAssault/Assets/Scripts/Player.cs
+++ ArgonAssault/Assets/Scripts/Player.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
-    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
-    [Tooltip("In ms")] [SerializeField] float xRange = 5f;
+    [Tooltip("In ms^-1")][SerializeField] float speed = 10f;
+    [Tooltip("In ms")] [SerializeField] float xRange = 8f;
+    [Tooltip("In ms")] [SerializeField] float yRange = 5.5f;
 
     // Use this for initialization
     void Start ()
@@ -17,11 +19,30 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xOffset = xThrow * xSpeed * Time.deltaTime;
-        float rawNewXPos = transform.localPosition.x + xOffset;
-        float clampedXpos = Mathf.Clamp(rawNewXPos, -5f, 5f);
+        ProcessTranslation();
+        ProcessRotation();
+    }
 
-        transform.localPosition = new Vector3(clampedXpos, transform.localPosition.y, transform.localPosition.z);
-	}
+    private void ProcessRotation()
+    {
+        transform.localRotation = Quaternion.Euler(-30f, 30f, 0f);
+    }
+
+    private void ProcessTranslation()
+    {
+        // Movement
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float yOffest = yThrow * speed * Time.deltaTime;
+
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float clampedXpos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+
+        float rawNewYPos = transform.localPosition.y + yOffest;
+        float clampedYPos = Mathf.Clamp(rawNewYPos, -yRange, yRange);
+
+        // Update position
+        transform.localPosition = new Vector3(clampedXpos, clampedYPos, transform.localPosition.z);
+    }
 }
