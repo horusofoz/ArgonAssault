adec4559fcb4868c44cf615a06afb3a68f503b3e
Jeff <horusofoz@gmail.com>
2018-04-25 11:53:31 +0930

    * Re-organised assets * Separated Scene Loader and Music Player

--- ArgonAssault/Assets/Scripts/MusicPlayer.cs
+++ ArgonAssault/Assets/Scripts/MusicPlayer.cs
@@ -1,24 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 public class MusicPlayer : MonoBehaviour {
 
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
-    }
-
-    // Use this for initialization
-    void Start ()
-    {    
-        Invoke("LoadFirstScene", 2f);
-	}
-	
-	// Load next scene
-    void LoadFirstScene()
-    {
-        SceneManager.LoadScene(1);
     }
 }
